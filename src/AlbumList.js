c929e2e1baed95dc5cd9757510293131c9d4397d
Paulo <psface1@gmail.com>
2017-12-17 03:24:00 -0200

    Creating AlbumList component

---
+++ src/AlbumList.js
@@ -0,0 +1,11 @@
+export default function renderAlbums(data, element) {
+  const markup = data.map(album => `
+  <div class="list-item">
+    <img src="${album.images[2].url}" alt="${album.name}" class="list-image">
+    <div class="list-description">
+      <p class="list-title">${album.name}</p>
+      <p class="list-subtitle">${album.artists[0].name}</p>
+    </div>
+  </div>`).join('');
+  element.innerHTML = markup;
+}
