5af1b3660bb2d41422f6df449e77870885e6c7a8
Paulo <psface1@gmail.com>
2017-12-20 00:43:52 -0200

    Creating AlbumTracks component

---
+++ src/AlbumTracks.js
@@ -0,0 +1,15 @@
+/* eslint no-param-reassign: ["error", { "props": false }] */
+
+function createMarkup(tracks) {
+  return tracks.map(track => `
+  <div class="music" data-track-preview="">
+    <p class="music-number">${track.track_number}</p>
+    <p class="music-title">${track.name}</p>
+    <p class="music-duration">${track.duration_ms}</p>
+  </div>`).join('');
+}
+
+export default function renderAlbumTracks(data, element) {
+  const markup = createMarkup(data);
+  element.innerHTML = markup;
+}
